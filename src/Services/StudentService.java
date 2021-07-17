b9cc5b58b7c114292eb254cef3a65a026b1e76e9
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-07-18 01:07:29 +0530

    update student service

--- src/Services/StudentService.java
+++ src/Services/StudentService.java
@@ -13,22 +13,6 @@ public class StudentService {
     private static final File dbFile = new File("Student.db");
     private static List<Student> studentsList = new ArrayList<>();
 
-
-    //    static{
-//
-//        /* Let's add some dummy data */
-//        Student s1 = new Student("945678124v", "Sadun ", "Colombo", "077-1234567", "abc@ijse.lk");
-//        Student s2 = new Student("961710065v", "Dulanga ", "Galle", "077-1234567", "abc@ijse.lk");
-//        Student s3 = new Student("965678135v", "Nuwan ", "Matara", "077-1234567", "abc@ijse.lk");
-//        Student s4 = new Student("975675674v", "Amara ", "Kandy", "077-1234567", "abc@ijse.lk");
-//        Student s5 = new Student("941358124v", "Kamal ", "Jaffna", "077-1234567", "abc@ijse.lk");
-//        studentsDB.add(s1);
-//        studentsDB.add(s2);
-//        studentsDB.add(s3);
-//        studentsDB.add(s4);
-//        studentsDB.add(s5);
-//    }
-
     static {
         readDataFromFile();
     }
